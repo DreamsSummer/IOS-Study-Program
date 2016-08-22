e6745b3a1de5cb046aefbc39c9a7e357cdcad0ee
DreamsSummer <779388649@qq.com>
2016-08-22 23:34:27 +0800

    对UIWindow的加载过程的补充

---
+++ UI进阶-控制器的创建-04UIWindow补充/UI进阶-控制器的创建-04UIWindow补充/AppDelegate.m
@@ -0,0 +1,60 @@
+//
+//  AppDelegate.m
+//  UI进阶-控制器的创建-04UIWindow补充
+//
+//  Created by liser on 16/8/21.
+//  Copyright © 2016年 liser. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    
+    
+    self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
+    
+    
+//    application.windows 只要一给delegate的window属性赋值，就会添加到windows数组
+    
+    self.window.backgroundColor = [UIColor greenColor];
+    
+    UIViewController *rv = [[UIViewController alloc]init];
+    
+    self.window.rootViewController = rv;
+    
+    //makeKeyAndVisible:成为app的主窗口并且显示
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+@end
---
+++ UI进阶-控制器的创建-04UIWindow补充/UI进阶-控制器的创建-04UIWindow补充/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UI进阶-控制器的创建-04UIWindow补充
+//
+//  Created by liser on 16/8/21.
+//  Copyright © 2016年 liser. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
