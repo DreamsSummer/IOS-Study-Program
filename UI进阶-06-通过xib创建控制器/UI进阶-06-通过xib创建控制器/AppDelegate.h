2930baece0c70ef446ceb7af5f3fe6aaefeb7b34
DreamsSummer <779388649@qq.com>
2016-08-22 23:35:39 +0800

    描述通过xib的形式创建控制器的过程

---
+++ UI进阶-06-通过xib创建控制器/UI进阶-06-通过xib创建控制器/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UI进阶-06-通过xib创建控制器
+//
+//  Created by liser on 16/8/22.
+//  Copyright © 2016年 liser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UI进阶-06-通过xib创建控制器/UI进阶-06-通过xib创建控制器/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UI进阶-06-通过xib创建控制器
+//
+//  Created by liser on 16/8/22.
+//  Copyright © 2016年 liser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
