b23745461dfc48a7dabd1961c3b36794511ed78c
DreamsSummer <779388649@qq.com>
2016-08-23 23:48:45 +0800

    对导航控制器的简单使用

---
+++ UI进阶-控制器创建-10导航控制器的简单使用/UI进阶-控制器创建-10导航控制器的简单使用/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UI进阶-控制器创建-10导航控制器的简单使用
+//
+//  Created by liser on 16/8/23.
+//  Copyright © 2016年 liser. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
