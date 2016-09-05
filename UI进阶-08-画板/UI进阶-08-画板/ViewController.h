fbcb3ae6c6861e8d59dcf6a71e29f5dda90d1f52
DreamsSummer <779388649@qq.com>
2016-09-05 19:26:01 +0800

    画板功能

---
+++ UI进阶-08-画板/UI进阶-08-画板/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UI进阶-08-画板
+//
+//  Created by liser on 16/9/4.
+//  Copyright © 2016年 liser. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UI进阶-08-画板/UI进阶-08-画板/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UI进阶-08-画板
+//
+//  Created by liser on 16/9/4.
+//  Copyright © 2016年 liser. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
